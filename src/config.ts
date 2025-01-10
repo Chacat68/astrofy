4b63c34d568fd9d003ce92bfda70f3dbf249d80b
chacat68 <cainefox@gmail.com>
2025-01-10 17:43:49 +0800

    添加关于我的信息，更新个人作品集网站的介绍内容

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,10 @@ import { z, defineCollection } from "astro:content";
 
 export const SITE_TITLE = 'CHACAT | 个人作品集';
 export const SITE_DESCRIPTION = '网站会展示我的作品，技能，以及我的职业经历。';
+export const ABOUT_ME = `Hey there 👋
+我是CHACAT
+一个游戏开发者和个人公司实践者
+这是我的个人作品集网站，希望你喜欢！`;
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
 export const SHOP_ENABLED = false; // 控制商店功能开关
